fda8b9ef51c8ccf862da50192eab4b200f8b8391
s1m0nj <si@simonjackson.info>
2014-10-30 23:22:45 +0000

    Added Show Hide Low Value toggle button

--- tampermonkey-harvestapp-reports-editprojectbutton.user.js
+++ tampermonkey-harvestapp-reports-editprojectbutton.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name       Harvest reports improvements for harvestapp.com, see www.getharvest.com
 // @namespace  http://www.prosperity247.com/
-// @version    0.22
-// @description  Add's additional buttons into the DOM that allows us to navigate around harvest more easily
+// @version    0.23
+// @description  Add's addtional buttons into the DOM that allows us to navigate around harvest more easily
 // @match      https://*.harvestapp.com/reports*
 // @copyright  2014 Simon Jackson Prosperity 24.7
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
@@ -21,7 +21,7 @@ $(function()
   {
       console.log('Harvest Reports improvements');
       
-     
+      
       
       console.log('Add Edit Project Button');
       
@@ -39,39 +39,56 @@ $(function()
           t.parent().prepend('&nbsp;<a href="'+editProjectUrl+'" target="_blank" class="btn-action btn-small">Edit Project</a>'); 
       });
       
-            
+      
       console.log('Add Client Invices Button');
       function getClientIdFromUrl(url){
           return url.between('/reports/clients/','?');
       }
       $(".ur-name a[href*=client],.pb-name a[href*=client],.td-item a[href*=client]")
-        .each(function() {
+      .each(function() {
           var t =  $(this);
           var url = t.attr('href');
           
           var clientid = getClientIdFromUrl(url);
           
           var clientinvoiceUrl = '/invoices/archive?invoice_report=selected&timeframe=all&client='+clientid+'&status%5B%5D=all';
-		  t.parent().parent().find('td:nth-child(2)').html('<a href="'+clientinvoiceUrl+'" target="_blank" class="btn-action btn-small">Previous Invoices</a>'); 
+          t.parent().parent().find('td:nth-child(2)').html('<a href="'+clientinvoiceUrl+'" target="_blank" class="btn-action btn-small" style="font-weight:normal;">Previous Invoices</a>'); 
           
       });
       
-          
+      
       $("a.btn-action[href*=invoice]").each(function() {
           $(this).attr('target','_blank');
       }); 
       
       console.log('Add Top Toolbar Buttons');
-      var button = $('<a class="btn-action btn-small btn-chrome"><span>Remove Zero Items</span></a>')
+      var zeroButton = $('<span><a class="btn-action btn-small btn-chrome"><span>Hide/Show 0.00 Uninvoiced Hours</span></a></span>')
       .click(function () {
           $('.ur-unhours a')
-          .filter(function(index) { return $(this).text() === "0.00"; })
+          .filter(function() { return $(this).text() === "0.00"; })
           .each(function() {
-              $(this).closest('tr').remove();
+              $(this).closest('tr').toggle();
           });
       });
       
-      $(".f-right").append(button);
+      var lowValueButton = $('<span><a class="btn-action btn-small btn-chrome"><span>Hide/Show Low Value</span></a></span>');
+      lowValueButton.click(function () {
+          $('.ur-unamount')
+          .slice(1) //Skip header row
+          .filter(function() {
+              var text = $(this).text();
+              var number = Number(text.replace(/[^0-9\.]+/g,""));
+              return number < 300.00;
+          })
+          .each(function() {
+              $(this).closest('tr').toggle();
+          });
+      });
+      
+      var toolbar = $(".f-right");
+      toolbar.append(zeroButton);
+      toolbar.append(" ");
+      toolbar.append(lowValueButton);
   }
  );
 
