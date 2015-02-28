0dcef746e4ab41c827e840e0e3125af1da5f728f
s1m0nj <si@simonjackson.info>
2015-02-28 17:33:12 +0000

    Extended for new invoice screen
    It sets to my defaults for normal invoices, also on click of new
    invoice project it will select the text incase you want to copy that
    content for the subject on the new invoice

--- tampermonkey-harvestapp-reports-editprojectbutton.user.js
+++ tampermonkey-harvestapp-reports-editprojectbutton.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name       Harvest reports improvements for harvestapp.com, see www.getharvest.com
 // @namespace  http://www.prosperity247.com/
-// @version    0.23
-// @description  Add's addtional buttons into the DOM that allows us to navigate around harvest more easily
+// @version    0.25
+// @description  Add"s addtional buttons into the DOM that allows us to navigate around harvest more easily
 // @match      https://*.harvestapp.com/reports*
+// @match      https://*.harvestapp.com/invoices/new*
 // @copyright  2014 Simon Jackson Prosperity 24.7
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // ==/UserScript==
@@ -19,76 +20,141 @@ String.prototype.between = function(prefix, suffix) {
 
 $(function() 
   {
-      console.log('Harvest Reports improvements');
-      
-      
-      
-      console.log('Add Edit Project Button');
+      console.log("Harvest Reports Improvements");
+      console.log("Add Edit Project Button");
       
       function getProjectIdFromUrl(url){
-          return url.between('/reports/projects/','?');
+          return url.between("/reports/projects/","?");
       }
       
       $(".ur-name a[href*=projects],.pb-name a[href*=projects],.td-item a[href*=projects]").each(function() {
           var t =  $(this);
-          var url = t.attr('href');
+          var url = t.attr("href");
           
           var projectid = getProjectIdFromUrl(url);
           
-          var editProjectUrl = '/projects/'+projectid+'/edit';
-          t.parent().prepend('&nbsp;<a href="'+editProjectUrl+'" target="_blank" class="btn-action btn-small">Edit Project</a>'); 
+          var editProjectUrl = "/projects/"+projectid+"/edit";
+          t.parent().prepend("&nbsp;<a href='"+editProjectUrl+"' target='_blank' class='btn-action btn-small'>Edit Project</a>"); 
       });
       
       
-      console.log('Add Client Invices Button');
+      console.log("Add Client Buttons");
       function getClientIdFromUrl(url){
-          return url.between('/reports/clients/','?');
+          return url.between("/reports/clients/","?");
       }
       $(".ur-name a[href*=client],.pb-name a[href*=client],.td-item a[href*=client]")
       .each(function() {
           var t =  $(this);
-          var url = t.attr('href');
+          var url = t.attr("href");
           
           var clientid = getClientIdFromUrl(url);
           
-          var clientinvoiceUrl = '/invoices/archive?invoice_report=selected&timeframe=all&client='+clientid+'&status%5B%5D=all';
-          t.parent().parent().find('td:nth-child(2)').html('<a href="'+clientinvoiceUrl+'" target="_blank" class="btn-action btn-small" style="font-weight:normal;">Previous Invoices</a>'); 
+          var clientinvoiceUrl = "/invoices/archive?invoice_report=selected&timeframe=all&client="+clientid+"&status%5B%5D=all";
+          var clientEstimatesUrl = "/estimates/archive?timeframe=all&client="+clientid+"&status%5B%5D=all";
+          t.parent().parent().find("td:nth-child(2)").html("<a href='"+clientinvoiceUrl+"' target='_blank' class='btn-action btn-small' style='font-weight:normal;'> Invoices </a>&nbsp;<a href='"+clientEstimatesUrl+"' target='_blank' class='btn-action btn-small' style='font-weight:normal;'>Estimates</a>"); 
           
       });
       
-      
       $("a.btn-action[href*=invoice]").each(function() {
-          $(this).attr('target','_blank');
+          $(this).attr("target","_blank");
       }); 
       
-      console.log('Add Top Toolbar Buttons');
-      var zeroButton = $('<span><a class="btn-action btn-small btn-chrome"><span>Hide/Show 0.00 Uninvoiced Hours</span></a></span>')
-      .click(function () {
-          $('.ur-unhours a')
-          .filter(function() { return $(this).text() === "0.00"; })
-          .each(function() {
-              $(this).closest('tr').toggle();
-          });
-      });
+      console.log("Add Top Toolbar Buttons");
       
-      var lowValueButton = $('<span><a class="btn-action btn-small btn-chrome"><span>Hide/Show Low Value</span></a></span>');
-      lowValueButton.click(function () {
-          $('.ur-unamount')
-          .slice(1) //Skip header row
-          .filter(function() {
-              var text = $(this).text();
-              var number = Number(text.replace(/[^0-9\.]+/g,""));
-              return number < 300.00;
-          })
-          .each(function() {
-              $(this).closest('tr').toggle();
-          });
-      });
+      switch (window.location.pathname) {
+          case "/reports/uninvoiced":
+              var zeroButton = $("<span><a class='btn-action btn-small btn-chrome'><span>Hide/Show 0.00 Uninvoiced Hours</span></a></span>")
+              .click(function () {
+                  $(".ur-unhours a")
+                  .filter(function() { 
+                      return $(this).text() === "0.00"; 
+                  })
+                  .each(function() {
+                      $(this).closest("tr").toggle();
+                  });
+              });
+              
+              var lowValueButton = $("<span><a class='btn-action btn-small btn-chrome'><span>Hide/Show Low Value</span></a></span>");
+              lowValueButton.click(function () {
+                  $(".ur-unamount")
+                  // .slice(1) //Skip header row
+                  .filter(function() {
+                      var text = $(this).text();
+                      var number = Number(text.replace(/[^0-9\.]+/g,""));
+                      return number < 300.00;
+                  })
+                  .each(function() {
+                      $(this).closest("tr").toggle();
+                  });
+              });
+              
+              var toolbar = $(".f-right");
+              toolbar.append(zeroButton);
+              toolbar.append(" ");
+              toolbar.append(lowValueButton);              
+              break;
+          case "/invoices/new":
+              
+              //wait for in visible on id invoice_configure_projects
+              var invoice_configure_projects = $("#invoice_configure_projects");
+              
+              if ($(invoice_configure_projects).is(":visible")){
+                  setupNewInvoiceSettings();
+              } 
+              else{
+                  $("#select_retainer_button").click(setupNewInvoiceSettings)
+              }
+              
+              break;
+      }
       
-      var toolbar = $(".f-right");
-      toolbar.append(zeroButton);
-      toolbar.append(" ");
-      toolbar.append(lowValueButton);
+      
+      function setupNewInvoiceSettings(){
+          
+          if($(".select_project_checkbox").length > 1){
+              $(".select_project_checkbox").each( function() {
+                  var project = $(this);
+                  project.prop("checked", false);
+                  
+                  var label = $(project.parent().find("label"));
+                  var element = label.text(label.text().trim());
+                  project.click(function() {
+                      var selection = window.getSelection();        
+                     var range = document.createRange();
+                      range.selectNodeContents(label[0]);
+                      selection.removeAllRanges();
+                      selection.addRange(range);
+                  });
+              } );
+          }
+          
+          //Create date string s UK format.
+          var aYearAgo = new Date();
+          aYearAgo.setDate(aYearAgo.getDate() - 365);
+          aYearAgo = aYearAgo.getDate() + "/" + aYearAgo.getMonth() + "/" + aYearAgo.getFullYear();
+          var yesterday = new Date();
+          yesterday.setDate(yesterday.getDate()- 1);
+          yesterday = yesterday.getDate() + "/" + yesterday.getMonth() + "/" + yesterday.getFullYear();
+          
+          $("#import_hours").prop("checked", true);
+          $("select#timeframe").val("Custom");
+          $("select#timeframe").trigger("onchange");
+          $("input#start_date").val(aYearAgo);
+          $("input#end_date").val(yesterday);
+          
+          $("input#invoice_type_summary_detailed").prop("checked", true);
+          $("input#invoice_type_summary_detailed").trigger( "click");
+          $("input[name='include-0project']").prop("checked", false);
+          $("input[name='include-4date']").prop("checked", true);
+          $("input[name='include-1task']").prop("checked", false);
+          $("input[name='include-2people']").prop("checked", true);
+          $("input[name='include-3note']").prop("checked", true);
+          
+          $("input#import_all_expenses").prop("checked", true);
+          $("input#invoice_expense_summary_detailed").prop("checked", true);
+          $("input#invoice_expense_summary_detailed").trigger("click");
+          $("input[name='include-1category']").prop("checked", false);
+      }
   }
  );
 
